147b4ded5284ef5e3504958eb34118a7d6075677
devdewance@gmail.com <devdewance@gmail.com>
2016-09-13 23:39:32 +0530

    parashar devashish updated

--- src/main/java/com/pdp/demo/controller/IndexController.java
+++ src/main/java/com/pdp/demo/controller/IndexController.java
@@ -6,9 +6,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-	@RequestMapping("/")
-	public String estimate(){
-		return "welcome";
-	}
-	
+
 }
